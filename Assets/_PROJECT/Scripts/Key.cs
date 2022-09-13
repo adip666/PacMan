fe4f7026f7de28dfdc00e7fe9f1bc3bb4e4223db
adip666 <adrian.przydatek@hotmail.com>
2022-09-13 16:25:24 +0200

    Added safe places

--- Assets/_PROJECT/Scripts/Key.cs
+++ Assets/_PROJECT/Scripts/Key.cs
@@ -12,6 +12,7 @@
         public const string WALL = "Wall";
         public const string ENEMY = "Enemy";
         public const string PLAYER = "Player";
+        public const string SAFE_PLACES = "SafePlace";
     }
 
     public enum Levels
