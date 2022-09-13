90efc504312216a9a87d72c73c6a93dc5b46b1be
adip666 <adrian.przydatek@hotmail.com>
2022-09-13 17:09:04 +0200

    Added doors

---
+++ Assets/_PROJECT/Scripts/Door/OnlyForPlayerDoor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PacMan.Keys.Door
+{
+    public class OnlyForPlayerDoor : BaseDoor
+    {
+        protected override bool CanReact(Collider other)
+        {
+            return other.transform.CompareTag(Tag.PLAYER);
+        }
+    }
+}
